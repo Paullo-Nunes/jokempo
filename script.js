85ee51e19e00c439f6a94d8349e50a4fd7d2b883
Dev Js <alienwaredev369@outlook.com>
2026-07-11 10:01:28 -0300

    Pegando as escolhas tanto do usuário quanto da máquina e mostrando no console.

--- script.js
+++ script.js
@@ -2,4 +2,15 @@
 
 const playHuman = (humanChoice) => {
     console.log(humanChoice);
+    console.log(playMachine());
+}
+
+const playMachine = () => {
+    const choices = ['rock', 'paper', 'scissors'];
+
+    const randomNumber = Math.floor(Math.random() * choices.length);
+
+    console.log(randomNumber);
+
+    return choices[randomNumber];
 }
